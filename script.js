d88f533a3143ffa955242592818bd3cd04bda91c
goncharovastacy <gggnchr@gmail.com>
2023-05-06 18:15:37 +0700

    1st

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const button = document.getElementById('button');
+const text = document.getElementById('text');
+
+function workWithNumber () {
+    const input = document.getElementById('input').value;
+    //делим число на цифры и получаем массив
+    const arr = input.split('');
+    //получаем цифры
+    const numbers = arr.join(', ');
+    //получаем количество цифр
+    const numbersLength = input.length;
+    //вычисляем сумму цифр
+    const sum = arr.reduce((acc, item) => Number(acc) + Number(item));
+    //выводим цифры в обратном порядке
+    const reverse = arr.reverse().join('');
+
+    text.innerHTML = `Нам даны цифры: ${numbers} <br>
+    Цифр в числе: ${numbersLength} <br>
+    Сумма: ${sum} <br>
+    Обратный порядок: ${reverse}`;
+
+  }
+  
+  button.addEventListener('click', workWithNumber);
+  
